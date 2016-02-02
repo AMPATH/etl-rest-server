d9b51b1a2de3b9a79b94375d53b8a4b4e282e435
werick <jackercmugoma@gmail.com>
2016-02-02 17:13:50 +0300

    merge conflicts

--- etl-daemon.js
+++ etl-daemon.js
@@ -2,17 +2,31 @@ var Service = require('node-linux').Service;
 
 // Create a new service object
 var svc = new Service({
+<<<<<<< HEAD
     name:'Etl Server',
     description: 'The nodejs.org etl web server.',
     script: '/home/werick/etl-rest-server/etl-server.js',
     wait: 2,
     grow: .5
   });
+=======
+  name: 'Etl Server',
+  description: 'The nodejs.org etl web server.',
+  script: '/home/werick/etl-rest-server/etl-server.js',
+  wait: 2,
+  grow: .5
+});
+>>>>>>> 4292ecc... AA-449-fix ci testing bug
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
 svc.on('install', function() {
+<<<<<<< HEAD
     svc.start();
   });
+=======
+  svc.start();
+});
+>>>>>>> 4292ecc... AA-449-fix ci testing bug
 
 svc.install();
--- uninstall-deamon.js
+++ uninstall-deamon.js
@@ -2,6 +2,7 @@ var Service = require('node-linux').Service;
 
 // Create a new service object
 var svc = new Service({
+<<<<<<< HEAD
     name:'Etl Server',
     script: require('path').join('/home/werick/etl-rest-server/etl-server.js')
   });
@@ -11,6 +12,17 @@ svc.on('uninstall', function() {
     console.log('Uninstall complete.');
     console.log('The service exists: ', svc.exists);
   });
+=======
+  name: 'Etl Server',
+  script: require('path').join('/home/werick/etl-rest-server/etl-server.js')
+});
+
+// Listen for the "uninstall" event so we know when it's done.
+svc.on('uninstall', function() {
+  console.log('Uninstall complete.');
+  console.log('The service exists: ', svc.exists);
+});
+>>>>>>> 4292ecc... AA-449-fix ci testing bug
 
 // Uninstall the service.
 svc.uninstall();
