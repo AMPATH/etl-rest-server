b394cf4efc0a4bcbcdcbd24efdbac3da655a4930
werick <jackercmugoma@gmail.com>
2016-02-02 17:13:50 +0300

    AA-549:Feature-Revised calander view to use two datasets

--- uninstall-deamon.js
+++ uninstall-deamon.js
@@ -11,6 +11,5 @@ svc.on('uninstall', function() {
     console.log('Uninstall complete.');
     console.log('The service exists: ', svc.exists);
   });
-
 // Uninstall the service.
 svc.uninstall();
