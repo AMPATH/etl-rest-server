b00d1cbf86cd5a16013ade39a8ae3dd37adfa922
maikofelix89 <maikofelix89@gmail.com>
2018-12-10 11:08:58 +0300

    Fixed synce on first http request
    Fixed remove whitespace issue

--- etl-routes.js
+++ etl-routes.js
@@ -3043,10 +3043,12 @@ module.exports = function () {
                 config: {
                     auth: 'simple',
                     handler: function (request, reply) {
-                        if (config.eidSyncOn === true)
-                            eidLabData.getPatientLabResults(request, reply);
-                        else
+                        if (config.eidSyncOn === true){
+                           const labSyncService = new LabSyncService();
+                            labSyncService.syncAllLabsByPatientUuid(request.query.patientUuId, reply);
+                        }else {
                             reply(Boom.notImplemented('Sorry, sync service temporarily unavailable.'));
+                        }
                     }
                 }
             },
