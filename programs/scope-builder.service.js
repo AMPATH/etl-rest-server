9d6b2d8eb8c7ba440f91cefca23c3a37a34b3336
Dennis Kigen <kigen.work@gmail.com>
2019-08-23 14:09:49 +0300

    POCAMRS-273: Add validation to PEP visit

--- programs/scope-builder.service.js
+++ programs/scope-builder.service.js
@@ -92,7 +92,61 @@ function isInitialPrepVisit(patientEncounters) {
     return encounter.encounterType.uuid === initialPrEPEncounterUuid;
   });
 
-  return previousPrEPEncounters.length === 0;
+  return initialPrEPEncounters.length === 0;
+}
+
+function isInitialPepVisit(patientEncounters) {
+  const initialPEPEncounterUuid = 'c3a78744-f94a-4a25-ac9d-1c48df887895';
+  let initialPEPEncounters = [];
+
+  initialPEPEncounters = _.filter(patientEncounters, (encounter) => {
+    return encounter.encounterType.uuid === initialPEPEncounterUuid;
+  });
+
+  return initialPEPEncounters.length === 0;
+}
+
+function isInitialOncologyVisit(encounters, programUuid) {
+  const oncologyProgramEncounterTypeMap = [
+    {
+      programUuid: 'e8bc5036-1462-44fa-bcfe-ced21eae2790', // Lung Cancer Treatment Program
+      initialEncounterUuid: 'be7b0971-b2ab-4f4d-88c7-e7322aa58dbb' // Lung Cancer Initial
+    },
+    {
+      programUuid: '725b5193-3452-43fc-aca3-6a80432d9bfa', // General Oncology Program
+      initialEncounterUuid: 'd17b3adc-0837-4ac6-862b-0953fc664cb8' // General Oncology Initial
+    },
+    {
+      programUuid: '88566621-828f-4569-9af5-c54f8237750a', // Breast Cancer Treatment Program
+      initialEncounterUuid: '9ad5292c-14c3-489b-9c14-5f816e839691' // Breast Cancer Initial
+    },
+    {
+      programUuid: 'e48b266e-4d80-41f8-a56a-a8ce5449ebc6', // Sickle Cell Program
+      initialEncounterUuid: 'ba5a15eb-576f-496b-a58d-e30b802a5da5' // Sickle Cell Initial
+    },
+    {
+      programUuid: 'a3610ba4-9811-46b3-9628-83ec9310be13', // Hemophilia Program
+      initialEncounterUuid: '3945005a-c24f-478b-90ec-4af84ffcdf6b' // Hemophilia Initial
+    },
+    {
+      programUuid: '698b7153-bff3-4931-9638-d279ca47b32e', // Multiple Myeloma Program
+      initialEncounterUuid: 'bf762b3e-b60a-436a-a40b-f874c59869ec' // Multiple Myeloma Initial
+    },
+    {
+      programUuid: '418fe011-a903-4862-93d4-5e7c84d9c253', // Anticoagulation Program
+      initialEncounterUuid: '4a7450b1-f720-4a0c-b13b-d8a6a83348ee' // Anticoagulation Initial
+    }
+  ];
+
+  let initialOncologyEncounters = [];
+  let initialEncounterType = oncologyProgramEncounterTypeMap.find(e => e.programUuid === programUuid);
+  
+  if (initialEncounterType) {
+    initialOncologyEncounters = _.filter(encounters, encounter => {
+      return initialEncounterType.initialEncounterUuid === encounter.encounterType.uuid
+    });
+  }
+  return initialOncologyEncounters.length === 0;
 }
 
 function buildProgramScopeMembers(scope, programEnrollment) {
@@ -121,6 +175,7 @@ function buildHivScopeMembers(scope, lastTenHivSummary, intendedVisitLocationUui
     scope.isFirstAMPATHHIVVisit = !isIntraTransfer(lastTenHivSummary, intendedVisitLocationUuid);
     scope.previousHIVClinicallocation = null;
   }
-  // It's a first PrEP visit if the patient has no previous PrEP encounters
+  
   scope.isFirstPrEPVisit = isInitialPrepVisit(scope.patientEncounters);
+  scope.isFirstPEPVisit = isInitialPepVisit(scope.patientEncounters);
 }
