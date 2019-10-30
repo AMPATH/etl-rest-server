fa85fdac0da6acf049d43510b0747180e6d23a86
jecihjoy <jecihjoy@gmail.com>
2019-10-30 16:09:14 +0300

    DQA: added nutrition assessment to DQA reports

--- app/reporting-framework/DQA/dqa-chart-abstraction-service.js
+++ app/reporting-framework/DQA/dqa-chart-abstraction-service.js
@@ -4,10 +4,10 @@ export class DQAChartAbstractionService {
 
     constructor() { }
 
-    getDQAChartAbstractionReport(locations, limit, offset) {
+    getDQAChartAbstractionReport(locations, limit, offset, startDate, endDate) {
         let dao = this.getDaoClass();
         return new Promise((resolve, reject) => {
-            return dao.getDQAChartAbstractionReport(locations, limit, offset)
+            return dao.getDQAChartAbstractionReport(locations, limit, offset, startDate, endDate)
                 .then((results) => {
                     resolve({
                         results: results
