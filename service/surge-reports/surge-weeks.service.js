8b66d563be25d9123e152f477cd0841aed9fed43
jacinta254 <jecihjoy@gmail.com>
2021-01-04 14:33:36 +0300

    code cleanup

--- service/surge-reports/surge-weeks.service.js
+++ service/surge-reports/surge-weeks.service.js
@@ -3,7 +3,6 @@ const Promise = require('bluebird');
 
 const surgeWeeksDao = {
   getSurgeWeeks: () => {
-    console.log('CALLED');
     return new Promise((resolve, reject) => {
       let sql = 'select * from etl.surge_week order by start_date desc';
       let queryParts = {
