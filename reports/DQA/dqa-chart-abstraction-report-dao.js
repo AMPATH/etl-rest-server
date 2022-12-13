3e4315072338d173e1acf2235d0e846e956cb60a
Kipchumba C. Bett <kbett68@gmail.com>
2022-12-13 14:58:41 +0300

    Enhancements: DQA report improvements

--- reports/DQA/dqa-chart-abstraction-report-dao.js
+++ reports/DQA/dqa-chart-abstraction-report-dao.js
@@ -3,97 +3,117 @@ import QueryService from '../../app/database-access/query.service';
 export class DQAChartAbstractionDAO {
   constructor() {}
 
-  getDQAChartAbstractionReport(locations, limit, offset) {
+  getDQAChartAbstractionReport(
+    locations,
+    limit,
+    offset,
+    startDate,
+    endDate,
+    patientType
+  ) {
+    let where = '';
+    switch (patientType) {
+      case 'PEADS':
+        where = `EXTRACT(YEAR FROM (FROM_DAYS(DATEDIFF(NOW(), h.birthdate)))) < 15`;
+        break;
+      case 'PMTCT':
+        where = `p.program_id = 4`;
+        break;
+      default:
+        where = `EXTRACT(YEAR FROM (FROM_DAYS(DATEDIFF(NOW(), h.birthdate)))) >= 15`;
+    }
     let runner = this.getSqlRunner();
     let sqlQuery =
-      `SELECT
-      GROUP_CONCAT(DISTINCT identifier
-                   SEPARATOR ', ') AS identifiers,
-      uuid,
-      person_id,
-      NUPI,
-      ccc_number,
-      person_name,
-      birthdate,
-      gender,
-      hiv_start_date                                                                                  as date_confirmed_hiv_positive,
-      arv_start_date,
-      vl_1,
-      cd4_1,
-      enrolled_in_dc,
-      last_clinical_encounter,
-      IF(timestampdiff(year, birthdate, now()) > 0, round(timestampdiff(year, birthdate, now()), 0),
-         round(timestampdiff(month, birthdate, now()) / 12, 2))                                       as age,
-      encounter_datetime                                                                              as last_appointment_date,
-      rtc_date                                                                                        as next_appointment,
-      etl.get_arv_names(cur_arv_meds)                                                                 AS drugs_given,
-      weight,
-      height,
-      IF(height is not null and weight is not null, weight / ((height / 100) * (height / 100)), null) as BMI,
-      condom_provided_this_visit,
-      tb_screened_this_visit,
-      ipt_start_date,
-      ipt_stop_date,
-      ipt_completion_date,
-      tb_screen
-FROM (SELECT h.uuid,
-            h.person_id,
-            CONCAT(COALESCE(person_name.given_name, ''), ' ', COALESCE(person_name.middle_name, ''), ' ',
-                   COALESCE(person_name.family_name, ''))             AS person_name,
-            id.identifier,
-            cc.identifier                                             as ccc_number,
-            inu.identifier                                            as NUPI,
-            gender,
-            pp.program_id,
-            birthdate,
-            encounter_id,
-            encounter_datetime,
-            rtc_date,
-            h.hiv_start_date,
-            h.arv_start_date,
-            h.location_id,
-            h.cd4_1,
-            h.vl_1,
-            h.weight,
-            h.height,
-            IF(is_clinical_encounter = 1, encounter_datetime, null)   AS last_clinical_encounter,
-            is_clinical_encounter,
-            cur_arv_meds,
-            tb_screen,
-            tb_screening_datetime,
-            IF((pp.program_id is not null AND pp.date_completed is null), 1,0) as enrolled_in_dc,
-            IF(tb_screening_datetime = encounter_datetime, 1, 0)      AS tb_screened_this_visit,
-            ipt_start_date,
-            IF(ipt_start_date = encounter_datetime, 1, 0)             AS ipt_started_this_visit,
-            ipt_stop_date,
-            ipt_completion_date,
-            IF(ipt_stop_date = encounter_datetime
-                   OR ipt_completion_date = encounter_datetime, 1, 0) AS ipt_ended_this_visit,
-            condoms_provided_date,
-            IF(condoms_provided_date = encounter_datetime, 1, 0)      AS condom_provided_this_visit
-     FROM etl.flat_hiv_summary_v15b h
-              INNER JOIN amrs.person t1 ON (h.person_id = t1.person_id)
-              INNER JOIN amrs.person_name person_name ON (t1.person_id = person_name.person_id
-         AND (person_name.voided = 0
-             || person_name.voided = 0))
-         LEFT JOIN amrs.patient_program pp on (t1.person_id = pp.patient_id and pp.program_id = 9 AND pp.voided = 0)
-              LEFT JOIN amrs.patient_identifier id ON (t1.person_id = id.patient_id
-         AND (id.voided = 0))
-              INNER JOIN amrs.patient_identifier inu ON (t1.person_id = inu.patient_id
-         AND (inu.voided = 0 AND inu.identifier_type = 45))
-              INNER JOIN amrs.patient_identifier cc ON (t1.person_id = cc.patient_id
-         AND (cc.voided = 0 AND cc.identifier_type = 28))
-     WHERE is_clinical_encounter = 1
-       AND encounter_datetime > '2018-01-01'
-       AND h.location_id IN (` +
+      `
+    SELECT
+          h.person_uuid as uuid,
+          cc.identifier as person_id,
+          np.identifier as NUPI,
+          EXTRACT(YEAR FROM (FROM_DAYS(DATEDIFF(NOW(), h.birthdate)))) AS age,
+          IF( date(h.tb_screening_datetime) > date_sub(h.endDate, interval 6 month),"YES","NO") as tb_screened_this_visit,
+          h.gender as 'sex_gender',
+          e.height,
+          e.weight,
+          DATE_FORMAT(h.birthdate,'%Y-%m-%d') AS birthdate,
+          h.encounter_id,
+          DATE_FORMAT(h.encounter_date,'%Y-%m-%d') as last_appointment_date,
+          h.rtc_date,
+          h.location_id,
+          h.cur_arv_meds,
+          h.tb_screen,
+          h.tb_screening_datetime,
+          hiv_start_date,
+          h.arv_start_date,
+          cd4_1,
+         IF(is_clinical_encounter = 1, encounter_datetime, null)   AS last_clinical_encounter,
+          DATE_FORMAT(h.rtc_date,'%Y-%m-%d') as next_appointment,
+          h.vl_1,
+          case
+             when e.weight is not null and e.height is not null then 'YES'
+            else 'NO'
+                end as nutrition,
+          case
+          when e.visit_type in (37,58) then 'CARG'
+                        when e.visit_type in (43,80,104,118,120,123) then 'Treatment Supporter'
+            else 'Self'
+                end as visit_type,
+        h.cur_arv_meds_names AS drugs_given,
+          case
+          when e.height is not null and e.weight is not null then e.weight/((e.height/100) * (e.height/100))
+          else null
+        end as BMI,
+          IF(p.program_id = 9, 'DC', 'Standard') AS DSD,
+          TIMESTAMPDIFF(DAY,h.encounter_date,h.rtc_date) AS drugs_duration,
+          IF(h.ipt_start_date = h.encounter_date,
+              1,
+              0) AS ipt_started_this_visit,
+          DATE_FORMAT(h.ipt_start_date,'%Y-%m-%d') AS last_ipt_start_date,
+          h.ipt_stop_date,
+          h.ipt_completion_date,
+          IF(h.ipt_stop_date = h.encounter_date
+                  OR h.ipt_completion_date = h.encounter_date,
+              1,
+              0) AS ipt_ended_this_visit
+      FROM
+          etl.hiv_monthly_report_dataset_frozen h
+          left join etl.flat_hiv_summary_v15b e on (h.encounter_id=e.encounter_id)
+              INNER JOIN
+          amrs.person t1 ON (h.person_id = t1.person_id)
+            INNER JOIN amrs.person_name person_name ON (t1.person_id = person_name.person_id
+              AND (person_name.voided = 0
+                  || person_name.voided = 0))
+              LEFT JOIN
+          amrs.patient_identifier id ON (t1.person_id = id.patient_id
+              AND id.voided = 0)
+              LEFT JOIN
+        amrs.patient_identifier cc ON (t1.person_id = cc.patient_id and cc.identifier_type in (28)
+              AND cc.voided = 0)
+              LEFT JOIN
+        amrs.patient_identifier np ON (t1.person_id = np.patient_id and np.identifier_type in (45)
+              AND np.voided = 0)
+        left join amrs.patient_program p on (p.patient_id = h.person_id and p.program_id in (4,9) and p.date_completed is null and p.voided = 0)
+        WHERE
+        h.status = "active"
+        AND e.height IS NOT NULL
+        AND e.weight IS NOT NULL
+      AND h.endDate >= '` +
+      startDate +
+      `'
+      AND h.endDate <= '` +
+      endDate +
+      `'
+      AND h.location_id IN (` +
       locations +
-      `)
-     ORDER BY encounter_datetime DESC) clinical
-GROUP BY person_id DESC limit ` +
+      `) AND ` +
+      where +
+      ` 
+    GROUP BY h.person_id
+    ORDER BY RAND() DESC limit ` +
       limit +
       ` offset ` +
       offset +
-      `;`;
+      `; `;
+    console.log('Query: ', sqlQuery);
     return new Promise((resolve, reject) => {
       runner
         .executeQuery(sqlQuery)
