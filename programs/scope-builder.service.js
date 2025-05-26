1275ce2b1969d03ffd2920b6cdd0067c972c8476
kantush <sainingo254@gmail.com>
2025-05-26 11:48:58 +0300

    Adjust Workflow for 'Referral' and 'Linkage' Forms

--- programs/scope-builder.service.js
+++ programs/scope-builder.service.js
@@ -22,7 +22,9 @@ function buildScope(dataDictionary) {
     showOnlyHTSINITIAL: false,
     showOnlyHTSRetest: false,
     showHTSRetestToConfirmP: false,
-    showOthersHTSEncounters: false
+    showOthersHTSEncounters: false,
+    showHTSReferral: false,
+    showHTSLinkage: false
   };
   let isStandardDcVisit = false;
 
@@ -641,6 +643,10 @@ function buildHTSScopeMembers(
     patientEncounters,
     HTS_ENCOUNTER_TYPES.RETEST
   );
+  const latestReferral = getLatestEncounterOfType(
+    patientEncounters,
+    HTS_ENCOUNTER_TYPES.REFERRAL
+  );
 
   const hasOldScreening =
     latestScreening && isFromPreviousDate(latestScreening);
@@ -649,14 +655,26 @@ function buildHTSScopeMembers(
     latestScreening && !isFromPreviousDate(latestScreening);
   const hasInitialToday = latestInitial && !isFromPreviousDate(latestInitial);
   const hasRetestToday = latestRetest && !isFromPreviousDate(latestRetest);
+  const hasReferralToday =
+    latestReferral && !isFromPreviousDate(latestReferral);
 
   if (isHtsPatientNegative && hasInitialToday) {
     scope.showOnlyHTSINITIAL = true;
     return scope;
   }
 
+  if (!hasScreeningToday && hasRetestToday) {
+    if (!hasReferralToday) {
+      scope.showHTSReferral = true;
+    } else {
+      scope.showHTSLinkage = true;
+    }
+    return scope;
+  }
+
   if (!hasScreeningToday) {
     scope.showOnlyHTSScreening = true;
+    scope.showHTSRetestToConfirmP = true;
     scope.isFirstHTSNewRetestVisit = true;
   } else if (hasScreeningToday && !hasInitialToday) {
     scope.showOnlyHTSINITIAL = true;
@@ -664,7 +682,11 @@ function buildHTSScopeMembers(
   } else if (hasInitialToday && !hasRetestToday) {
     scope.showOnlyHTSRetest = true;
   } else if (hasRetestToday) {
-    scope.showOthersHTSEncounters = true;
+    if (!hasReferralToday) {
+      scope.showHTSReferral = true;
+    } else {
+      scope.showOthersHTSEncounters = true;
+    }
   } else {
     scope.showOnlyHTSScreening = true;
   }
