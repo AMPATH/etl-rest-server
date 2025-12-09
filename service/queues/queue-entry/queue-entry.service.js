b4a49e8f59c1119c12c677b5f4baf50d241cce4f
maikofelix47 <maikofelix89@gmail.com>
2025-12-09 21:59:32 +0300

    (improv) Add visit uuid

--- service/queues/queue-entry/queue-entry.service.js
+++ service/queues/queue-entry/queue-entry.service.js
@@ -11,6 +11,7 @@ export class ServiceEntry {
       const sql = `SELECT 
     q.name,
     qe.queue_entry_id,
+    qe.uuid AS 'queue_entry_uuid',
     q.uuid AS 'service_uuid',
     q.location_id,
     l.name AS 'location',
@@ -33,14 +34,15 @@ export class ServiceEntry {
     CASE
         WHEN qe.priority = 11666 THEN 'NORMAL'
         WHEN qe.priority = 12360 THEN 'EMERGENCY'
-    END AS 'priority'
+    END AS 'priority',
+    v.uuid AS 'visit_uuid'
 FROM
     amrs.queue_entry qe
         JOIN
     amrs.queue q ON (qe.queue_id = q.queue_id)
         JOIN
     amrs.concept c ON (q.service = c.concept_id)
-        JOIN
+        LEFT JOIN
     amrs.queue_room qr ON (qr.queue_id = q.queue_id)
         JOIN
     amrs.location l ON (q.location_id = l.location_id)
@@ -48,11 +50,12 @@ FROM
     amrs.person p ON (p.person_id = qe.patient_id)
         JOIN
     amrs.person_name pn ON (pn.person_id = p.person_id)
+        JOIN
+    amrs.visit v ON (v.visit_id = qe.visit_id)
 WHERE
     qe.ended_at IS NULL
         AND c.uuid = '${serviceUuid}'
-        AND l.uuid = '${locationUuid}'
-	group by qe.patient_id,q.queue_id;`;
+        AND l.uuid = '${locationUuid}';`;
       const queryParts = {
         sql: sql
       };
