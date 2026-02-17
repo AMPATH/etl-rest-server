ecf5b41a5143090afde849f88ace83879aabd12a
Denzel Kipkemoi <denzelkipkemoi@Denzels-MacBook-Pro.local>
2026-02-17 15:57:56 +0300

    updated route parameters

--- etl-routes.js
+++ etl-routes.js
@@ -6744,7 +6744,7 @@ module.exports = (function () {
       config: {
         auth: 'default',
         handler: async function (request, reply) {
-          const locationUuid = request.query.location_uuid;
+          const locationUuid = request.query.locationUuid;
           const supersetService = new SupersetService();
           try {
             await loadAndMaplocationUuidToId();
