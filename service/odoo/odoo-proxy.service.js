8777e6f4f050313e0ffea718d04c7a8caa665254
Manu <enyachoke@ampath.org>
2026-04-16 10:34:55 +0300

    Add /etl/odoo/billing/orders

--- service/odoo/odoo-proxy.service.js
+++ service/odoo/odoo-proxy.service.js
@@ -63,7 +63,42 @@ function getBillingByOrder(orderId) {
   });
 }
 
+/**
+ * List sale orders with optional filters.
+ * Calls GET /ampath/billing/orders
+ *
+ * @param {object} filters
+ * @param {string}  [filters.company_external_id] - Location UUID of the facility
+ * @param {string}  [filters.date_from]           - ISO date YYYY-MM-DD (inclusive)
+ * @param {string}  [filters.date_to]             - ISO date YYYY-MM-DD (inclusive)
+ * @param {string}  [filters.state]               - draft|sent|sale|done|cancel
+ * @param {number}  [filters.limit]               - Max records (default 100)
+ * @param {number}  [filters.offset]              - Pagination offset (default 0)
+ */
+function listOrders(filters) {
+  var odooConfig = getOdooConfig();
+  var qs = {};
+  if (filters) {
+    if (filters.company_external_id)
+      qs.company_external_id = filters.company_external_id;
+    if (filters.date_from) qs.date_from = filters.date_from;
+    if (filters.date_to) qs.date_to = filters.date_to;
+    if (filters.state) qs.state = filters.state;
+    if (filters.limit !== undefined) qs.limit = filters.limit;
+    if (filters.offset !== undefined) qs.offset = filters.offset;
+  }
+  return rp({
+    method: 'GET',
+    uri: odooConfig.host + '/ampath/billing/orders',
+    qs: qs,
+    headers: authHeaders(odooConfig),
+    json: true,
+    rejectUnauthorized: false
+  });
+}
+
 module.exports = {
   getBillingByPatient: getBillingByPatient,
-  getBillingByOrder: getBillingByOrder
+  getBillingByOrder: getBillingByOrder,
+  listOrders: listOrders
 };
