6ac8a4cf3c0d332c8da272d506d9444eb1725574
Felix Maiko <maikofelix89@gmail.com>
2026-04-23 14:05:59 +0300

    (improv) MOH-740 indicator addition and patient list (#1526)

--- app/routes/moh-740.route.js
+++ app/routes/moh-740.route.js
@@ -41,6 +41,46 @@ const routes = [
       tags: ['api'],
       validate: {}
     }
+  },
+  {
+    method: 'GET',
+    path: '/moh-740-report/patient-list',
+    config: {
+      handler: function (request, reply) {
+        if (request.query.locationUuids) {
+          (request.query.reportName = 'moh-740-report'),
+            preRequest.resolveLocationIdsToLocationUuids(request, function () {
+              let requestParams = Object.assign(
+                {},
+                request.query,
+                request.params
+              );
+
+              let requestCopy = _.cloneDeep(requestParams);
+              let reportParams = etlHelpers.getReportParams(
+                request.query.reportName,
+                ['startDate', 'endDate', 'locationUuids', 'locations'],
+                requestParams
+              );
+              let report = 'MOH-740-report';
+              requestCopy.locations = reportParams.requestParams.locations;
+              const moh740Service = new Moh740Service(report, requestCopy);
+
+              moh740Service
+                .getPatientListReport(requestParams)
+                .then((results) => {
+                  reply(results);
+                })
+                .catch((err) => {
+                  reply(Boom.internal('An error occured', err));
+                });
+            });
+        }
+      },
+      description: 'MOH-412 Report',
+      notes: 'Returns Report for HIV Cervical Cancer Screening',
+      tags: ['api']
+    }
   }
 ];
 
