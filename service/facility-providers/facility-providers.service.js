e34e12e95e86c103dff8094c1d7cb5265be58af3
Felix Maiko <maikofelix89@gmail.com>
2026-06-10 18:23:49 +0300

    (feat) Add facility-providers service (#1543)

---
+++ service/facility-providers/facility-providers.service.js
@@ -0,0 +1,42 @@
+const db = require('../../etl-db');
+
+export class FacilityProvidersService {
+  constructor() {}
+
+  getFacilityActiveProvidersByLocationUuid(locationUuid, serviceUuid) {
+    if (!locationUuid) {
+      throw new Error('Location not defined');
+    }
+    return new Promise((resolve, reject) => {
+      const sql = `SELECT 
+        e.location_id,
+        l.name AS location_name,
+        ep.provider_id,
+        CONCAT(pn.given_name, "  ", pn.middle_name, " ",  pn.family_name) AS provider_names,
+        pa.value_reference AS national_id
+      FROM amrs.encounter e 
+      INNER JOIN amrs.location l ON e.location_id = l.location_id
+      INNER JOIN amrs.encounter_provider ep ON e.encounter_id = ep.encounter_id
+      INNER JOIN amrs.provider pr ON ep.provider_id = pr.provider_id
+      INNER JOIN amrs.person_name pn ON (pn.person_id = pr.person_id)
+      INNER JOIN amrs.provider_attribute pa ON (
+        pa.provider_id = pr.provider_id AND pa.attribute_type_id = 5
+      )
+      WHERE 
+        e.encounter_datetime BETWEEN DATE_SUB(NOW(), INTERVAL 12 MONTH) AND NOW()
+        AND e.voided = 0
+        AND pn.voided = 0
+        AND l.uuid = '${locationUuid}'
+        AND pr.retired = 0
+        AND (e.voided IS NULL OR e.voided = 0)
+      GROUP BY pr.provider_id;`;
+      const queryParts = {
+        sql: sql
+      };
+      db.queryServer(queryParts, function (result) {
+        result.sql = sql;
+        resolve(result.result);
+      });
+    });
+  }
+}
