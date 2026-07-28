76274e10ff46a3cb708a36e55aac63f582d2f67c
Felix Maiko <maikofelix89@gmail.com>
2026-07-28 22:21:35 +0300

    (feat) Add facility admission requests

--- app/routes/admission.route.js
+++ app/routes/admission.route.js
@@ -1,4 +1,7 @@
-import { getPatientAdmissionHistory } from '../../service/admissions/admissions-service';
+import {
+  getPatientAdmissionHistory,
+  getAdmissionRequests
+} from '../../service/admissions/admissions-service';
 var Boom = require('boom');
 const routes = [
   {
@@ -20,6 +23,29 @@ const routes = [
       },
       description: 'Get patient admission history ',
       notes: 'Returns all patient admission history',
+      tags: ['api'],
+      validate: {}
+    }
+  },
+  {
+    method: 'GET',
+    path: '/etl/admissions/requests',
+    config: {
+      handler: async function (request, reply) {
+        const locationUuid = request.query.locationUuid
+          ? request.query.locationUuid
+          : null;
+        try {
+          const results = await getAdmissionRequests(locationUuid);
+          reply({
+            results: results
+          });
+        } catch (error) {
+          reply(Boom.badRequest());
+        }
+      },
+      description: 'Get ward admission requests',
+      notes: 'Returns a facility admisssion requests',
       tags: ['api'],
       validate: {}
     }
