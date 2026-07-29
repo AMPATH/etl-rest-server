3737d8fbc56d9984e44a6f5afb55e7c3a0a81a19
Felix Maiko <maikofelix89@gmail.com>
2026-07-29 08:49:04 +0300

    (feat) Add facility admission requests (#1568)

--- service/admissions/admissions-service.js
+++ service/admissions/admissions-service.js
@@ -49,4 +49,70 @@ WHERE
     });
   });
 }
-module.exports = { getPatientAdmissionHistory };
+function getAdmissionRequests(locationUuid) {
+  if (!locationUuid) {
+    throw new Error('Location not defined');
+  }
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT 
+    obs.obs_datetime AS 'admission_request_date',
+    l.name AS 'admission_location',
+    CONCAT(cr.identifier, ' , ', id.identifier) AS 'identifiers',
+    UPPER(CONCAT_WS(' ',
+                    pn.given_name,
+                    pn.middle_name,
+                    pn.family_name)) AS patient_name,
+    EXTRACT(YEAR FROM (FROM_DAYS(DATEDIFF(NOW(), p.birthdate)))) AS age,
+    p.gender,
+    l.uuid AS location_uuid,
+    p.uuid patient_uuid,
+    v.uuid AS visit_uuid,
+    vt.name AS 'visit_type'
+FROM
+    amrs.obs obs
+        JOIN
+    amrs.obs al ON (obs.obs_group_id = al.obs_group_id
+        AND al.concept_id = 12659
+        AND obs.voided = 0)
+        JOIN
+    amrs.encounter re ON (re.encounter_id = obs.encounter_id)
+        JOIN
+    amrs.visit v ON (v.visit_id = re.visit_id)
+        JOIN
+    amrs.visit_type vt ON (vt.visit_type_id = v.visit_type_id)
+        JOIN
+    amrs.location l ON (l.location_id = al.value_text)
+        JOIN
+    amrs.person p ON (p.person_id = obs.person_id
+        AND p.voided = 0)
+        INNER JOIN
+    amrs.person_name pn ON (pn.person_id = p.person_id
+        AND pn.voided = 0)
+        LEFT JOIN
+    amrs.encounter ae ON (ae.encounter_type = 318
+        AND ae.encounter_datetime > obs.obs_datetime
+        AND ae.voided = 0)
+        LEFT JOIN
+    amrs.patient_identifier cr ON (cr.patient_id = obs.person_id
+        AND cr.identifier_type = 55
+        AND cr.voided = 0)
+        LEFT JOIN
+    amrs.patient_identifier id ON (id.patient_id = obs.person_id
+        AND id.identifier_type = 5
+        AND id.voided = 0)
+WHERE
+    obs.concept_id = 12658
+        AND obs.value_coded = 12797
+        AND obs.voided = 0
+        AND l.uuid = '${locationUuid}'
+        AND ae.encounter_id IS NULL;`;
+    const queryParts = {
+      sql: sql
+    };
+    db.queryServer(queryParts, function (result) {
+      result.sql = sql;
+      resolve(result.result);
+    });
+  });
+}
+module.exports = { getPatientAdmissionHistory, getAdmissionRequests };
